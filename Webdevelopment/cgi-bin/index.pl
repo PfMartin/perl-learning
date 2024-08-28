f53a90cb18948ab18e51035d3d7ee70f2695b9ec
PfMartin <martinpfatrisch@gmail.com>
2024-08-28 20:02:49 +0200

    Added more functionality like forms to the webdevelopment folder

--- Webdevelopment/cgi-bin/index.pl
+++ Webdevelopment/cgi-bin/index.pl
@@ -14,14 +14,31 @@ sub print_index_page {
     my $user     = $CGI->param("user");
     my $password = $CGI->param("pwd");
 
+    my $query = $CGI->param("query");
+
+    my @query = $CGI->param();
+    @query = map( sprintf( "$_: %s", $CGI->param($_) ), @query );
+
+    my $params = join( ', ', @query );
+
     print( $CGI->header() );
     print <<HTML;
 <html>
     <b>Hello World!</b>
+    <h2>URL parameters</h2>
     <ul>
         <li>User: $user</li>
         <li>Password: $password</li>
     </ul>
+    
+    <h2>Basic form</h2>
+    <form action="index.pl" method="post">
+        <input type="text" name="query"/>
+        <input type="hidden" name="go" value="true" />
+        <input type="submit" name="submit" value="Go" />
+    </form>
+
+    <p>Last submitted: $params</p>
 </html>
 HTML
 }
